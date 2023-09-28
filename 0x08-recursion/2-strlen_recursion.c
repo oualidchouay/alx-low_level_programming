7b6998847800d1a71dff6d1e9b1563fc1cb4c8fe
oualidchouay <oualidchouay@gmail.com>
2023-09-28 05:23:30 -0700

    modify task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strlen_recursion - returns the length of a string.
+ * _strlen_recursion - returns the length of a string.
  * @s: pointer to string
  * Return: void
  */
