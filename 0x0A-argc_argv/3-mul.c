e9026b25a3e5e5f70c390bdfb1c5b66bd57d5849
oualidchouay <oualidchouay@gmail.com>
2023-10-04 06:10:56 -0700

    argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -multiplies two numbers.
+ * @argc: An argument counter
+ * @argv: An argument values
+ * Return: 0 on (success), 1 on (failure)
+ **/
+
+int main (int argc, char *argv[])
+{
+	int mul;
+
+	if(argc != 0)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul)
+		return 0;
+	}
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+File 0-Write a program that prints its name.
+File 1-Write a program that prints the number of arguments passed into it.
+File 2-Write a program that prints all arguments it receives.
+File 3-Write a program that multiplies two numbers.
+File 4-Write a program that adds positive numbers.
+File 5-Write a program that prints the minimum number of coins to make change for an amount of money.
