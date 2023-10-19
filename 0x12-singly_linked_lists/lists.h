5a9a51f5199f6d39e8ea2193f60cee1ceed5784a
oualidchouay <oualidchouay@gmail.com>
2023-10-19 13:21:17 -0700

    modify lists.h header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,55 +25,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/**
- * print_list - prints the contents of a linked list
- * @h: pointer to the head of the linked list
- *
- * Description: prints the contents of a linked list, one node per line
- *
- * Return: number of nodes printed
- */
 size_t print_list(const list_t *h);
-
-/**
- * list_len - counts the number of nodes in a linked list
- * @h: pointer to the head of the linked list
- *
- * Description: counts the number of nodes in a linked list
- *
- * Return: number of nodes in the linked list
- */
 size_t list_len(const list_t *h);
-
-/**
- * add_node - adds a new node to the beginning of a linked list
- * @head: pointer to the head of the linked list
- * @str: string to add to the new node
- *
- * Description: adds a new node to the beginning of a linked list
- *
- * Return: pointer to the new head of the linked list
- */
 list_t *add_node(list_t **head, const char *str);
-
-/**
- * add_node_end - adds a new node to the end of a linked list
- * @head: pointer to the head of the linked list
- * @str: string to add to the new node
- *
- * Description: adds a new node to the end of a linked list
- *
- * Return: pointer to the new tail of the linked list
- */
 list_t *add_node_end(list_t **head, const char *str);
-
-/**
- * free_list - frees all of the nodes in a linked list
- * @head: pointer to the head of the linked list
- *
- * Description: frees all of the nodes in a linked list, including the strings
- * that they contain
- */
 void free_list(list_t *head);
 
 #endif
