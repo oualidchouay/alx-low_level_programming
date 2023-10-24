9dbb0abb12b2aec28933cff0a5db3d26a1a1696c
oualidchouay <oualidchouay@gmail.com>
2023-10-24 10:14:20 -0700

    fix some errors

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,15 +7,16 @@
  *
  * Return: The number of nodes in the list.
  */
+
 size_t listint_len(const listint_t *h)
 {
-	size_t node_count = 0;
+	size_t num_nodes = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		num_nodes += 1;
 		h = h->next;
-		node_count++;
 	}
-	return (node_count);
+
+	return (num_nodes);
 }
