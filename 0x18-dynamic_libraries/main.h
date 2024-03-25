b5d44cc9c6f0ad18604d6a65ead287a3e809cae5
oualidchouay <oualidchouay@gmail.com>
2024-03-25 18:16:20 +0000

    errors

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,9 @@
 #ifndef __MAIN__
 #define __MAIN__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,7 +24,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int nrandom();
 
 
 #endif /* __MAIN__ */
